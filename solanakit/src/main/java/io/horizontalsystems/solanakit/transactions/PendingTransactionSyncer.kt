90793979136f680a6ccac39546b42812c5e1cdc9
LongWen <416441008@qq.com>
2025-02-21 17:35:25 +0800

    remove unnecessary libraries

--- solanakit/src/main/java/io/horizontalsystems/solanakit/transactions/PendingTransactionSyncer.kt
+++ solanakit/src/main/java/io/horizontalsystems/solanakit/transactions/PendingTransactionSyncer.kt
@@ -1,6 +1,5 @@
 package io.horizontalsystems.solanakit.transactions
 
-import android.util.Log
 import com.solana.api.Api
 import com.solana.models.ConfirmedTransaction
 import com.solana.rxsolana.api.getBlockHeight
@@ -8,15 +7,10 @@ import com.solana.rxsolana.api.getConfirmedTransaction
 import com.solana.rxsolana.api.getSlot
 import io.horizontalsystems.solanakit.database.transaction.TransactionStorage
 import io.horizontalsystems.solanakit.models.Transaction
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx2.await
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.withTimeout
 import org.sol4k.RpcUrl
-import org.web3j.protocol.Web3j
-import org.web3j.protocol.http.HttpService
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -29,11 +23,6 @@ class PendingTransactionSyncer(
     private val transactionManager: TransactionManager
 ) {
     private val logger = Logger.getLogger("PendingTransactionSyncer")
-
-    val web3j by lazy {
-        val builder = HttpService.getOkHttpClientBuilder()
-        Web3j.build(HttpService("https://solana-mainnet.core.chainstack.com/35d90e70a574359e50b11e10b5460414", builder.build()))
-    }
 
     suspend fun sync() {
         val updatedTransactions = mutableListOf<Transaction>()
