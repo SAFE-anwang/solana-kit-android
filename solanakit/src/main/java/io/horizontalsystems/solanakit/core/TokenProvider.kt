d1826c08d8c1bc044ed972607a233b19879f8d00
Long <416441008@qq.com>
2026-03-18 21:38:21 +0800

    fix bug

--- solanakit/src/main/java/io/horizontalsystems/solanakit/core/TokenProvider.kt
+++ solanakit/src/main/java/io/horizontalsystems/solanakit/core/TokenProvider.kt
@@ -1,8 +1,9 @@
 package io.horizontalsystems.solanakit.core
 
 import io.horizontalsystems.solanakit.transactions.JupiterApiService
+import io.horizontalsystems.solanakit.transactions.SolanaFmService
 
-class TokenProvider(private val jupiterApiService: JupiterApiService) {
+class TokenProvider(private val jupiterApiService: SolanaFmService) {
 
     suspend fun getTokenInfo(mintAddress: String) = jupiterApiService.tokenInfo(mintAddress)
 
